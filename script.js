f93d30b6df6c947a89e9d4686d51308e0f96de10
alex16-csharp <akyalex16@yahoo.com>
2023-03-08 18:49:38 +0200

    barman slider

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let slideIndex = [1, 1, 1, 1, 1, 1, 1, 1];
-let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8"]
+let slideIndex = [1, 1, 1, 1, 1, 1, 1, 1, 1];
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9"]
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
@@ -8,6 +8,7 @@ showSlides(1, 4);
 showSlides(1, 5);
 showSlides(1, 6);
 showSlides(1, 7);
+showSlides(1, 8);
 
 function plusSlides(n, no) {
     showSlides(slideIndex[no] += n, no);
