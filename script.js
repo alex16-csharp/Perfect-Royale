549e5ff0b554ad1517f168769e957baddd05dfd8
alex16-csharp <akyalex16@yahoo.com>
2023-03-09 18:44:34 +0200

    tort slider

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let slideIndex = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9", "mySlides10", "mySlides11", "mySlides12", "mySlides13", "mySlides14", "mySlides15", "mySlides16", "mySlides17", "mySlides18", "mySlides19", "mySlides20", "mySlides21", "mySlides22", "mySlides23"]
+let slideIndex = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9", "mySlides10", "mySlides11", "mySlides12", "mySlides13", "mySlides14", "mySlides15", "mySlides16", "mySlides17", "mySlides18", "mySlides19", "mySlides20", "mySlides21", "mySlides22", "mySlides23", "mySlides24"]
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
@@ -22,6 +22,7 @@ showSlides(1, 18);
 showSlides(1, 19);
 showSlides(1, 20);
 showSlides(1, 22);
+showSlides(1, 23);
 
 function plusSlides(n, no) {
     showSlides(slideIndex[no] += n, no);
