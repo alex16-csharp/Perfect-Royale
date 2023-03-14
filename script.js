efb1f6c1319bdc84c98390ddaa5a4c09074ef521
alex16-csharp <akyalex16@yahoo.com>
2023-03-14 13:47:58 +0200

    gif+ scroll up button fix

--- script.js
+++ script.js
@@ -40,22 +40,30 @@ function showSlides(n, no) {
     x[slideIndex[no] - 1].style.display = "block";
 }
 
-// up btn
-let mybutton = document.getElementById("upBtn");
+//Disable right mouse click Script
+//By Maximus (maximus@nsimail.com) w/ mods by DynamicDrive
+//For full source code, visit http://www.dynamicdrive.com  
+var message = "Iti multumesc ca nu copiezi!!!!";
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
+///////////////////////////////////
+function clickIE4() {
+    if (event.button == 2) {
+        alert(message);
+        return false;
     }
 }
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+function clickNS4(e) {
+    if (document.layers || document.getElementById && !document.all) {
+        if (e.which == 2 || e.which == 3) {
+            alert(message); return false;
+        }
+    }
 }
+if (document.layers) {
+    document.captureEvents(Event.MOUSEDOWN);
+    document.onmousedown = clickNS4;
+}
+else if (document.all && !document.getElementById) {
+    document.onmousedown = clickIE4;
+}
+document.oncontextmenu = new Function("alert(message);return false")
